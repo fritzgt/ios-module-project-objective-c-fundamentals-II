eb5d923599d8d07e12f038f811117695dc64441d
exoticproductions <contactexoticproductions@gamail.com>
2021-06-15 10:41:51 +0300

    Add model file

---
+++ TimeTracker/Model/FGTTimedTask.h
@@ -0,0 +1,17 @@
+//
+//  FGTTimedTask.h
+//  TimeTracker
+//
+//  Created by FGT MAC on 6/15/21.
+//  Copyright © 2021 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FGTTimedTask : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/Model/FGTTimedTask.m
@@ -0,0 +1,13 @@
+//
+//  FGTTimedTask.m
+//  TimeTracker
+//
+//  Created by FGT MAC on 6/15/21.
+//  Copyright © 2021 Lambda School. All rights reserved.
+//
+
+#import "FGTTimedTask.h"
+
+@implementation FGTTimedTask
+
+@end
